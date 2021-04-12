afb6488207e11f8c97f94101672dee2849353575
Vince Enriquez <0411vince@gmail.com>
2021-04-11 23:20:22 -0700

    Added functionality to ensure that save button saves the note and removes note taking area and buttons and also to ensure that cancel button should remove note taking area and buttons without saving note

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,7 @@
 const DARK_MODE = 'dark';
 const LIGHT_MODE = 'light';
 const THEME = 'mode';
-const notesarray = [];
+const notesArray = []
 
 // functions to change light mode into dark mode and vise versa //
 // Clicking moon logo changes theme to dark; clicking sun logo changes theme back to light//
@@ -37,15 +37,16 @@ function applyTheme() {
 
 function defineNoteWritingSection()  {
 html = `
-<div class = "textarea">
-<div class = "textinput" contenteditable="true"></div>
-<button onclick = "saveNote()">save</button>
-<button onclick = "cleanUp()">cancel</button>
+<div id = "textarea">
+  <div id = "text" contenteditable="true"></div>
+  <button onclick = "saveNote()">save</button>
+  <button onclick = "cleanUp()">cancel</button>
 </div>
 `
 return html 
 }
 
+// function to add new note //
 function createContentWritingArea()  {
 const div = document.querySelector('#content')
 div.insertAdjacentHTML('beforeend', defineNoteWritingSection())
@@ -53,21 +54,38 @@ setCursor()
 }
 
 function setCursor()  {
-const div = document.querySelector('#note-area')
+const div = document.querySelector('#text')
 div.focus()
 }
 
+// function to save note and store notes on an Array //
 function saveNote()  {
-const div = document.querySelector('#note')
+const div = document.querySelector('#text')
 const title = div.firstChild.textContent
-notesArray.push(createNote(title, "body"))
+const body = convertDivstoString()
+notesArray.push(createNote(title, body))
 console.log(notesArray)
+cleanUp()
 }
 
 function createNote(title, body)  {
 return { title,body }
 }
 
+function convertDivstoString()  {
+let str = "";
+const divs = [...document.querySelectorAll('[contenteditable] > div:not(:first-child)')];
+ for (const i of divs) {
+ str += `${i.textContent}\n`
+ }
+ return str
+ }
+
+// function to delete note //
+function cleanUp()  {
+ const div = document.querySelector('#textarea')
+ div.remove()
+ }
 
 
 
@@ -90,59 +108,19 @@ return { title,body }
 
 
 
-// * function to delete notes * //
-// • function deleteNote(index)  {     
-// •	 let confirmDel = confirm(“You are deleting this note!!”);
-// •	       
-// •	    If (confirmDel == true)  {
-// •	           Let notes = localStorage.getitem(“notes”);
-// •	           If (notes == null)  {
-// •	                notesObj = [];
-// •	       }  else  {
-// •	              notesObj = JSON.parse(notes);
-// •	       }
-// •	  
-// •	       notesObj.splice(index, 1);
-// •	       localStorage.setItem(“notes”,  JSON.stringify(notesObj));
-// •	       showNotes()
-// •    })
 
 
-// * function to add new note * //
-// •	function addNote(e){
-// •	     e.preventDefault();
-// •	
-// •	     let newNote = {};
-// •	       
-// •	     let title = document.querySelector(‘.title’);
-// •	     let note = document.querySelector(‘.note’);
-// •	        
-// •	     if(title.value == ‘ ‘ || note.value == ‘ ‘ ){
-// •	            Return alert(‘Please enter both fields.’);
-// •	      }else{
-// •	            newNote.title = title.value;
-// •	            newNote.note = note.value;
-// •	      }
-// •	         
-// •	      title.value = ‘ ’;
-// •	      note.value = ‘ ‘;
-// •	        
-// •	      noteList.push(newNote);
-// •	      appendNotes();
-// •	      cancel.click();
-// •	        
-// •	}
 
 
-// * function to store notes in an Array * //
-//  •	function appendNotes(){
-//  •	       
-//  •	let notes = Array.from(document.querySelectorAll(.’noteItem’));
-//  •	if(notes.length > 0){
-//  •	    notes.forEach(note =>{
-//  •	        note.remove();
-//  •	    })
-//  •	 }    
+
+
+
+
+
+
+
+
+ 
 
 
                 
